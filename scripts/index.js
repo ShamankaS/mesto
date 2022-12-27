3fa2ec76102748c70fa38602c7def22a4294ba0a
Tata Slyadneva <79031563664@ya.ru>
2022-12-27 22:33:54 +0300

    hotfixes after review

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,20 @@
 let editButton = document.querySelector('.profile__edit-button');
-let formElement = document.querySelector('.popup');
-let closeIcon = document.querySelector('.popup__close-icon');
-let nameInput = document.querySelector('.popup__editform_type_name');
-let jobInput = document.querySelector('.popup__editform_type_intro');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__container');
+let closeIcon = document.querySelector('.popup__close-button');
+let nameInput = document.querySelector('.popup__edit-form_type_name');
+let jobInput = document.querySelector('.popup__edit-form_type_intro');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__intro');
 
 function openPopup () {
-    formElement.classList.add('popup_active');
+    popup.classList.add('popup_active');
     nameInput.value = `${profileName.textContent}`;
     jobInput.value = `${profileJob.textContent}`;
 }
 
 function closePopup () {
-    formElement.classList.remove('popup_active');
+    popup.classList.remove('popup_active');
 }
 
 function handleFormSubmit(evt) {
