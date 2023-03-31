9f066da722c133eeb87f21ac047f3697875d3392
Tata Slyadneva <79031563664@ya.ru>
2023-03-31 16:24:24 +0300

    changed class UserInfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,13 +1,15 @@
 export default class UserInfo {
-    constructor({nameSelector, jobSelector}) {
+    constructor({nameSelector, jobSelector, avatarSelector}) {
         this._name = document.querySelector(nameSelector);
         this._job = document.querySelector(jobSelector);
+        this._avatar = document.querySelector(avatarSelector);
     }
 
     getUserInfo = () => {
         const userInfo = {
             fullname: this._name.textContent,
-            job: this._job.textContent
+            job: this._job.textContent,
+            avatar: this._avatar.src
         };
         return userInfo;
     }
